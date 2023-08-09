b74c590af152efc65f1dba2873d72475272f1a94
kamilszczepanik <kszczepanikworkspace@gmail.com>
2023-08-09 20:47:23 +0200

    [feature] buildLoginForm helper function accepting provided custom data

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -7,15 +7,21 @@ import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
 import faker from 'faker'
 
+const buildLoginForm = ({username, password}) => {
+  return {
+    username: username || faker.internet.userName(),
+    password: password || faker.internet.password(),
+  }
+}
+
 test('submitting the form calls onSubmit with username and password', async () => {
   const handleSubmit = jest.fn()
-  const username = faker.internet.userName()
-  const password = faker.internet.password()
+  const {username, password} = buildLoginForm({password: 'abc'})
 
   render(<Login onSubmit={handleSubmit} />)
+
   await userEvent.type(screen.getByLabelText(/username/i), username)
   await userEvent.type(screen.getByLabelText(/password/i), password)
-
   await userEvent.click(screen.getByRole('button', {name: /submit/i}))
 
   expect(handleSubmit).toBeCalledWith({
